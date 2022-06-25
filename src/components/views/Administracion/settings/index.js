83cb67a0833a5b69729a7a63d0840181dc4f24c1
Gama <gamalielbj@gmail.com>
2022-06-24 19:53:00 -0500

    Finished carrousel config

---
+++ src/components/views/Administracion/settings/index.js
@@ -0,0 +1,14 @@
+const settings = {
+    level0: {
+        dots: true,
+        arrows: true,
+        infinite: true,
+        autoplay: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '10px',
+    },
+};
+
+export default settings;
