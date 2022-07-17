d3b9b20c3b860b4ba8bf55ddb81c57ac4b2a43c4
Gama <gamalielbj@gmail.com>
2022-07-17 12:17:18 -0500

    The rest of column

---
+++ src/components/views/Exports/data/foodGroups.js
@@ -0,0 +1,272 @@
+export const groupColumns1 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento1',
+        key: 'grupoAlimento1',
+        width: 50,
+    },
+];
+export const groupColumns2 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento2',
+        key: 'grupoAlimento2',
+        width: 50,
+    },
+];
+export const groupColumns3 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento3',
+        key: 'grupoAlimento3',
+        width: 50,
+    },
+];
+export const groupColumns4 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento4',
+        key: 'grupoAlimento4',
+        width: 50,
+    },
+];
+export const groupColumns5 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento5',
+        key: 'grupoAlimento5',
+        width: 50,
+    },
+];
+export const groupColumns6 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento6',
+        key: 'grupoAlimento6',
+        width: 50,
+    },
+];
+export const groupColumns7 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento7',
+        key: 'grupoAlimento7',
+        width: 50,
+    },
+];
+export const groupColumns8 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento8',
+        key: 'grupoAlimento8',
+        width: 50,
+    },
+];
+export const groupColumns9 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento9',
+        key: 'grupoAlimento9',
+        width: 50,
+    },
+];
+export const groupColumns10 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento10',
+        key: 'grupoAlimento10',
+        width: 50,
+    },
+];
+export const groupColumns11 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento11',
+        key: 'grupoAlimento11',
+        width: 50,
+    },
+];
+export const groupColumns12 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento12',
+        key: 'grupoAlimento12',
+        width: 50,
+    },
+];
+export const groupColumns13 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento13',
+        key: 'grupoAlimento13',
+        width: 50,
+    },
+];
+export const groupColumns14 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento14',
+        key: 'grupoAlimento14',
+        width: 50,
+    },
+];
+export const groupColumns15 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento15',
+        key: 'grupoAlimento15',
+        width: 50,
+    },
+];
+export const groupColumns16 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento16',
+        key: 'grupoAlimento16',
+        width: 50,
+    },
+];
+export const groupColumns17 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento17',
+        key: 'grupoAlimento17',
+        width: 50,
+    },
+];
+export const groupColumns18 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento18',
+        key: 'grupoAlimento18',
+        width: 50,
+    },
+];
+export const groupColumns19 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento19',
+        key: 'grupoAlimento19',
+        width: 50,
+    },
+];
+export const groupColumns20 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento20',
+        key: 'grupoAlimento20',
+        width: 50,
+    },
+];
+export const groupColumns21 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento21',
+        key: 'grupoAlimento21',
+        width: 50,
+    },
+];
+export const groupColumns22 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento22',
+        key: 'grupoAlimento22',
+        width: 50,
+    },
+];
+export const groupColumns23 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento23',
+        key: 'grupoAlimento23',
+        width: 50,
+    },
+];
+export const groupColumns24 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento24',
+        key: 'grupoAlimento24',
+        width: 50,
+    },
+];
+export const groupColumns25 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento25',
+        key: 'grupoAlimento25',
+        width: 50,
+    },
+];
+export const groupColumns26 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento26',
+        key: 'grupoAlimento26',
+        width: 50,
+    },
+];
+export const groupColumns27 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento27',
+        key: 'grupoAlimento27',
+        width: 50,
+    },
+];
+export const groupColumns28 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento28',
+        key: 'grupoAlimento28',
+        width: 50,
+    },
+];
+export const groupColumns29 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento29',
+        key: 'grupoAlimento29',
+        width: 50,
+    },
+];
+export const groupColumns30 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento30',
+        key: 'grupoAlimento30',
+        width: 50,
+    },
+];
+export const groupColumns31 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento31',
+        key: 'grupoAlimento31',
+        width: 50,
+    },
+];
+export const groupColumns32 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento32',
+        key: 'grupoAlimento32',
+        width: 50,
+    },
+];
+export const groupColumns33 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento33',
+        key: 'grupoAlimento33',
+        width: 50,
+    },
+];
+export const groupColumns34 = [
+    {
+        title: 'Grupo alimento',
+        dataIndex: 'grupoAlimento34',
+        key: 'grupoAlimento34',
+        width: 50,
+    },
+];
