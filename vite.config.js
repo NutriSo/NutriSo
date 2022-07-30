1a295a1d56b95f930cef752ea9d9638819ef6926
Gama <gamalielbj@gmail.com>
2022-07-30 13:57:34 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,26 @@ const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
+    console.log('Comand: ', command, '\nMode: ', mode);
+    if (mode === 'production') {
+        return {
+            base: './',
+            plugins: [react()],
+            resolve: {
+                browser: true,
+                dedupe: ['react'],
+                preferBuiltins: false,
+                alias: {
+                    './runtimeConfig': './runtimeConfig.browser',
+                    '@': path.resolve(__dirname, 'src'),
+                },
+            },
+            server: {
+                host: true,
+            },
+        };
+    }
+
     return {
         plugins: [react()],
         server: {
